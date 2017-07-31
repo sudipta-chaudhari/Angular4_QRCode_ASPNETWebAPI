6ee65ceb1c2698ed1d15aa2e115dc5a83472d437
bobby-ind <bobby_gzb@yahoo.com>
2017-07-31 19:11:51 +0530

    Commit all

---
+++ Angular4WebApi/app/app.module.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="../typings/index.d.ts" />
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { routes } from './app.router';
+import { AppComponent } from "./app.component";
+//Routed Components
+import { HomeComponent } from './home/home.component';
+import { QRCodeComponent } from './qrcode/qrcode.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        HttpModule,
+        routes
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        QRCodeComponent
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ Angular4WebApi/app/app.router.ts
@@ -0,0 +1,13 @@
+﻿import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QRCodeComponent } from './qrcode/qrcode.component';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'qrcode', component: QRCodeComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
